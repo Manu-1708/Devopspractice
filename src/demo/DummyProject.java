7b35d1d43bac1f5fd282f5f64bbe018b604d5446
B M <manoj.b-m@capgemini.com>
2024-12-08 20:35:15 +0530

    changes in file

---
+++ src/demo/DummyProject.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class DummyProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("selected files only");
+
+	}
+
+}
